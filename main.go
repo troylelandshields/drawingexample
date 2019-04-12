edaad9a38f9c496de6b730353fec5f9728815b64
troylelandshields <troy.leland.shields@gmail.com>
2019-04-11 19:50:07 -0600

    use easyinput and show an example of a loop

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
-import "github.com/troylelandshields/drawing"
+import (
+	"fmt"
+
+	"github.com/troylelandshields/drawing"
+	"github.com/troylelandshields/easyinput"
+)
 
 func main() {
-	canvas := drawing.NewCanvas(1000, 1000)
+	canvas := drawing.NewCanvas(2000, 1000)
 
+	canvas.Move(drawing.Left, 10)
+	canvas.Turn(drawing.Right)
+
+	numberOfHouses := 5
+	for i := 1; i <= numberOfHouses; i++ {
+		fmt.Println("Drawing house", i)
+		drawHouse(canvas)
+	}
+
+	fmt.Println("Name the file:")
+	fileName := easyinput.TakeInput()
+	canvas.SaveImage(fileName)
+}
+
+func drawHouse(canvas *drawing.Canvas) {
 	canvas.Move(drawing.Straight, 2)
 	canvas.DrawRectangle(drawing.Green, 1, 2)
 	canvas.Move(drawing.Backwards, 2)
@@ -15,5 +35,7 @@ func main() {
 	canvas.DrawRectangle(drawing.Red, 5, 5)
 	canvas.DrawTriangle(drawing.Blue, 5)
 
-	canvas.SaveImage("img.jpeg")
+	canvas.Move(drawing.Backwards, 5)
+	canvas.Move(drawing.Left, 9)
+	canvas.Turn(drawing.Left)
 }
